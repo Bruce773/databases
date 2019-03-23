7fd9e828f221865ea83d5239c1ba844b34abc6a4
Bruce <bruce@yogainternational.com>
2019-03-22 20:30:56 -0400

    adds preliminary tables

--- server/schema.sql
+++ server/schema.sql
@@ -4,6 +4,23 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  message TEXT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  user TEXT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  room TEXT,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
